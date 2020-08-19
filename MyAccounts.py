6a692341df06746251795fc61a0ac022ef9be827
Waheed Rahuman <waheed.rahuman@gmail.com>
2020-08-19 16:58:19 +0400

    Accounts

--- MyAccounts.py
+++ MyAccounts.py
@@ -1,16 +1,45 @@
+def ReadAccount(mode): # Mode 1 for Print line 1 
+    f = open("April.txt","r")
+    cnt = 0
+    totalExpense = 0.00
+    line = f.readline()
+    while line:
+        if(mode == 1):
+            print(line)
+        
+        line = f.readline()
+        cnt += 1
+
+        # 18/08/2020,Milk,20.00 tokens
+        tokens = line.split(",") 
+        count = 0
+        for token in tokens:
+            count = count + 1
+            if(count == 3):
+                token = token[:-2] #Remove the line break 
+                totalExpense = float(token) + totalExpense
+        #End of the While
+    print ("Your Total Expenses AED " + str(totalExpense))
+
+
+#Main Program
+  
+
+ReadAccount(0) # Print Total Expenses
+
+
+#Read from the User
+expense_date = input("Enter the date (dd/mm/yyyy):")
+expense = input("Enter the Expense details :-")
+amount = input("Enter the amount AED ")
+
 f = open("April.txt","a")
 
-expense_date = input("Enter the date :")
-expense = input("Enter the Expense details :")
-amount = input("Enter the amount :")
-
+#Write to the file
 f.write(expense_date + "," + expense + "," + str(amount))
-
 f.write("\n") # Line Break
-
 f.close()
 
-f = open("April.txt","r")
-
-print(f.read())
+#Read the Account File
+ReadAccount(1)
 
