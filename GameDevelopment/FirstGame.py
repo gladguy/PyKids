4433ee959da3a42c4572dab4b579ee1d9100c5a4
Waheed Rahuman <waheed.rahuman@gmail.com>
2020-09-08 00:25:04 +0400

    Load Image

--- GameDevelopment/FirstGame.py
+++ GameDevelopment/FirstGame.py
@@ -7,27 +7,28 @@ win = pygame.display.set_mode((500,500))
 #Labelling the Window
 pygame.display.set_caption("First Game")
 
-x = 50
-y = 50
+x = 250
+y = 250
 width = 40
-height = 60
-speed = 5
+height = 40
+speed = 1
 
 done = False
 while not done:
 
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Good Bye !!")
             done = True
 
     win.fill((0, 0, 0))
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]:    y = y - 1
-    if pressed[pygame.K_DOWN]:  y = y + 1
-    if pressed[pygame.K_LEFT]:  x = x - 1
-    if pressed[pygame.K_RIGHT]: x = x + 1
+
+    if pressed[pygame.K_UP]:    y = y - speed
+    if pressed[pygame.K_DOWN]:  y = y + speed
+    if pressed[pygame.K_LEFT]:  x = x - speed
+    if pressed[pygame.K_RIGHT]: x = x + speed
 
     pygame.draw.rect(win, (100,100,255), (x, y, width, height))
     pygame.display.flip()
